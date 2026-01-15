e1ae4ec66aeaf0572df97f16626bafe8cdc01bb3
kimwest00 <kimwest00@hufs.ac.kr>
2026-01-15 22:51:39 +0900

    add: lec2 문제

---
+++ lib/Week2_Flutter_Layout_Basic/hw_1_variable_practice.dart
@@ -0,0 +1,14 @@
+//
+// 함수를 사용해 인사 메시지를 출력해봅시다!
+// 1. 이름을 전달받는 함수를 만들어주세요
+// 2. 함수 안에서 인사 메시지를 출력해주세요
+//
+
+/// TODO: [printGreeting] 함수를 완성해주세요
+void printGreeting() {
+  // TODO
+}
+
+void main() {
+  printGreeting();
+}
