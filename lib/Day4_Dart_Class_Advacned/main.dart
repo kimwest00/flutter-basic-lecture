1743527017cfc10ae3a1ce7a9be93b329aff042a
JLS <jeonhyunwoo7@gmail.com>
2026-02-05 14:36:50 +0900

    ???

--- lib/Day4_Dart_Class_Advacned/main.dart
+++ lib/Day4_Dart_Class_Advacned/main.dart
@@ -14,29 +14,37 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 171, 0, 238)),
       // home: const MyHomePage(title: 'Flutter Demo Home Page'), //!! <- MyHomePage class에 작성한 코드를 테스트하고 싶다면 해당 라인의  주석을 해제해주세요!
       home: const SalesScreen(title: 'Sales Screen'),
+    )
     );
   }
 }
 
+class _MyWidgetState extends State<MyWidgetsta>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = AnimationController(vsync: this);
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}{
+
+}
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
